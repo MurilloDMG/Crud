0c720222695723a6f6b8d07f715f98f3f05bcc32
Diego <diego>
2022-07-04 19:48:29 -0500

    first commit

---
+++ js/nuevocliente.js
@@ -0,0 +1,66 @@
+(function() {
+    let DB;
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded', () => {
+       conectarDB();
+
+       formulario.addEventListener('submit', validarCliente);
+    });
+
+    
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        //Leer todos los inputs
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+        
+        if (nombre === '' || email === '' || telefono === '' || empresa === '' ){
+            imprirmirAlerta('Todos los campos son obligatorios', 'error');
+
+            return;
+        }
+
+        //Crear un objeto a la informacion
+
+        const cliente ={
+            nombre,
+            email,
+            telefono,
+            empresa,
+            
+        }
+        cliente.id = Date.now();
+        crearNuevoCliente(cliente);
+    }
+
+    function crearNuevoCliente(cliente){
+       const transaction = DB.transaction(['crm'],'readwrite');
+
+       const objectStore=transaction.objectStore('crm');
+
+       objectStore.add(cliente);
+
+       transaction.onerror = function(){
+         console.log('Hubo un error');
+         imprirmirAlerta('Hubo un error','error');
+
+       };
+
+       transaction.oncomplete = function(){
+        console.log('Cliente agregado');
+        imprirmirAlerta('El cliente se agrego correctamente')
+
+        setTimeout(() => {
+            window.location.href ='index.html';
+        }, 2500);
+
+      }
+    }
+
+    
+})();
